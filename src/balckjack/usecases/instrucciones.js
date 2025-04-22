47a30e72f8957e2c5c82ad23af390a5f1aaa34a6
marco3islas <marco3islas@gmail.com>
2025-04-22 17:55:13 -0600

    Mejorada la interaccion con el boton instrucciones

--- src/balckjack/usecases/instrucciones.js
+++ src/balckjack/usecases/instrucciones.js
@@ -1,11 +1,13 @@
-
 const btnCerrar = document.getElementById('cerrarInstrucciones');
 const divInstrucciones = document.getElementById('divInstrucciones');
+export const mostrarInstrucciones = () => {
+    if (divInstrucciones.style.display === 'none'){
+        divInstrucciones.style.display = 'block';
+    } else {
+        divInstrucciones.style.display = 'none';
+    }
 
-btnCerrar.addEventListener('click', () => {
-  divInstrucciones.style.display = 'none';
-});
-
-export const cerrarInstrucciones = () => {
-  divInstrucciones.style.display = 'block';
+    btnCerrar.addEventListener('click', () => {
+        divInstrucciones.style.display = 'none';
+    });
 }
